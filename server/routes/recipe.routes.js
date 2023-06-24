9cbdb735f3676f37356016da165f8beb23b33e69
Hans Raj <75563900+Hans-Raj12@users.noreply.github.com>
2023-06-24 17:19:20 +0500

    resolving commit issue

--- server/routes/recipe.routes.js
+++ server/routes/recipe.routes.js
@@ -25,88 +25,44 @@ let express = require('express'),
         }
     });
    
-    
-let Recipe = require('../models/Recipe');
-router.post('/add-recipe',(req, res) => {
-    const {title, description,instructions,ingredients } = req.body;
-    const recipe = new Recipe({
-        title,
-        description,
-        instructions,
-        ingredients,
-        imageFilePath:""
-    })
-    recipe.save().then(result => {
-        res.status(201).json({
-            message: "Recipe Added Successfully!",
+    let Recipe = require('../models/Recipe');
+    router.post('/add-recipe',(req, res) => {
+        const {title, description,instructions,ingredients } = req.body;
+        const recipe = new Recipe({
+            title,
+            description,
+            instructions,
+            ingredients,
+            imageFilePath:""
         })
-    }).catch(err => {
-        console.log(err),
-            res.status(500).json({
-                error: err
-         });
-    })
-});
-
-router.get('/', (req, res, next) => {  
-    Recipe.find().then(data => {
-      res.status(200).json({
-        recipes: data
-      });
+        recipe.save().then(result => {
+            res.status(201).json({
+                message: "Recipe Added Successfully!",
+            })
+        }).catch(err => {
+            console.log(err),
+                res.status(500).json({
+                    error: err
+             });
+        })
     });
-  }
-)
-
-router.delete('/:id', (req, res, next) => {
-    Recipe.deleteOne({ _id: req.params.id }).then(result => {
-      console.log(result);
-      res.status(200).json({ message: "Recipe deleted!" });
-    });
-  }
-)
-
-// router.post('/',(req, res) => {
-//     const { title, description, goalAmount,ngoEmail,ngoId } = req.body;
-   
-//     const url = req.protocol + '://' + req.get('host')
-//     const imageFilePath = req.file.filename;
-//     const fundraisingpost = new FundraisingPost({
-//         title,
-//         description,
-//         goalAmount,
-//         imageFilePath,
-//         raisedAmount:0,
-//         createdAt:new Date(),
-//         ngoEmail,
-//         ngoId
-//     });
-//     fundraisingpost.save().then(result => {
-//         res.status(201).json({
-//             message: "User registered successfully!",
-//         })
-//     }).catch(err => {
-//         console.log(err),
-//             res.status(500).json({
-//                 error: err
-//             });
-//     })
-// })
-// router.get('/', (req, res, next) => {
-
-//        Users.find({roleId:"3", active:true})
-//       .then(posts => {
-        
-//         res.status(200).json(posts);
-//       })
-//       .catch(error => {
-//         res.status(500).json({
-//           message: "An error occurred while retrieving posts.",
-//           error: error
-//         });
-//       });
-        
     
-//   });
+    router.get('/', (req, res, next) => {  
+        Recipe.find().then(data => {
+          res.status(200).json({
+            recipes: data
+          });
+        });
+      }
+    )
+    
+    router.delete('/:id', (req, res, next) => {
+        Recipe.deleteOne({ _id: req.params.id }).then(result => {
+          console.log(result);
+          res.status(200).json({ message: "Recipe deleted!" });
+        });
+      }
+    )
 
 
 module.exports = router;
